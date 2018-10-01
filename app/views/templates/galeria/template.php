9e4712e40c1f82063fb3011eec888fa8e1fb0b92
HarCoreWare <bernardocodu@gmail.com>
2018-09-30 20:33:12 -0500

    nueva seccion

--- app/views/templates/galeria/template.php
+++ app/views/templates/galeria/template.php
@@ -1,6 +1,55 @@
 <section class="p-0" id="portfolio">
       <div class="container-fluid p-0">
         <div class="row no-gutters popup-gallery">
+
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/auto2.png">
+              <img class="img-fluid" src="img/auto2.png" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Linea Automotriz
+                  </div>
+                  <div class="project-name">
+                    Productos Automotrices
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/auto2.png">
+              <img class="img-fluid" src="img/auto2.png" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Linea Domestica
+                  </div>
+                  <div class="project-name">
+                    Productos Domésticos
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/auto2.png">
+              <img class="img-fluid" src="img/auto2.png" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Linea Hotelera
+                  </div>
+                  <div class="project-name">
+                    Productos Hospitalarios
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+
           <div class="col-lg-4 col-sm-6">
             <a class="portfolio-box" href="img/auto2.png">
               <img class="img-fluid" src="img/auto2.png" alt="">
@@ -10,87 +59,13 @@
                     Category
                   </div>
                   <div class="project-name">
-                    Project Name
+                    Productos Industriales
                   </div>
                 </div>
               </div>
             </a>
           </div>
-          <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="img/auto2.png">
-              <img class="img-fluid" src="img/auto2.png" alt="">
-              <div class="portfolio-box-caption">
-                <div class="portfolio-box-caption-content">
-                  <div class="project-category text-faded">
-                    Category
-                  </div>
-                  <div class="project-name">
-                    Project Name
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="img/auto2.png">
-              <img class="img-fluid" src="img/auto2.png" alt="">
-              <div class="portfolio-box-caption">
-                <div class="portfolio-box-caption-content">
-                  <div class="project-category text-faded">
-                    Category
-                  </div>
-                  <div class="project-name">
-                    Project Name
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="img/auto2.png">
-              <img class="img-fluid" src="img/auto2.png" alt="">
-              <div class="portfolio-box-caption">
-                <div class="portfolio-box-caption-content">
-                  <div class="project-category text-faded">
-                    Category
-                  </div>
-                  <div class="project-name">
-                    Project Name
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="img/auto2.png">
-              <img class="img-fluid" src="img/auto2.png" alt="">
-              <div class="portfolio-box-caption">
-                <div class="portfolio-box-caption-content">
-                  <div class="project-category text-faded">
-                    Category
-                  </div>
-                  <div class="project-name">
-                    Project Name
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="img/auto2.png">
-              <img class="img-fluid" src="img/auto2.png" alt="">
-              <div class="portfolio-box-caption">
-                <div class="portfolio-box-caption-content">
-                  <div class="project-category text-faded">
-                    Category
-                  </div>
-                  <div class="project-name">
-                    Project Name
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
+
         </div>
       </div>
     </section>
