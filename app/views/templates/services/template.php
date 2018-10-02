6ddce278b2eaabcda5e1f67d7964f4524e2a0db6
HarCoreWare <bernardocodu@gmail.com>
2018-10-02 18:09:57 -0500

    normalizado

--- app/views/templates/services/template.php
+++ app/views/templates/services/template.php
@@ -10,32 +10,32 @@
       </div>
       <div class="container">
         <div class="row">
-          <div class="col-lg-3 col-md-6 text-center">
+          <div class="col-lg-6 col-md-6 text-center">
             <div class="service-box mt-5 mx-auto">
-              <i class="fas fa-4x fa-gem text-primary mb-3 sr-icon-1"></i>
-              <h3 class="mb-3">Sturdy Templates</h3>
-              <p class="text-muted mb-0">Our templates are updated regularly so they don't break.</p>
+              <i class="fas fa-4x fa-gem text-primary mb-3 sr-icon-2"></i>
+              <h3 class="mb-3">Lorem Ipsum</h3>
+              <p class="text-muted mb-0">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 text-center">
+          <div class="col-lg-6 col-md-6 text-center">
             <div class="service-box mt-5 mx-auto">
               <i class="fas fa-4x fa-paper-plane text-primary mb-3 sr-icon-2"></i>
-              <h3 class="mb-3">Ready to Ship</h3>
-              <p class="text-muted mb-0">You can use this theme as is, or you can make changes!</p>
+              <h3 class="mb-3">Lorem Ipsum</h3>
+              <p class="text-muted mb-0">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 text-center">
+          <div class="col-lg-6 col-md-6 text-center">
             <div class="service-box mt-5 mx-auto">
-              <i class="fas fa-4x fa-code text-primary mb-3 sr-icon-3"></i>
-              <h3 class="mb-3">Up to Date</h3>
-              <p class="text-muted mb-0">We update dependencies to keep things fresh.</p>
+              <i class="fas fa-4x fa-binoculars text-primary mb-3 sr-icon-3"></i>
+              <h3 class="mb-3">Lorem Ipsum</h3>
+              <p class="text-muted mb-0">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 text-center">
+          <div class="col-lg-6 col-md-6 text-center">
             <div class="service-box mt-5 mx-auto">
-              <i class="fas fa-4x fa-heart text-primary mb-3 sr-icon-4"></i>
-              <h3 class="mb-3">Made with Love</h3>
-              <p class="text-muted mb-0">You have to make your websites with love these days!</p>
+              <i class="fas fa-4x fa-gem text-primary mb-3 sr-icon-4"></i>
+              <h3 class="mb-3">Loerm Ipsum</h3>
+              <p class="text-muted mb-0">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
             </div>
           </div>
         </div>
